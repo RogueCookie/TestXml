c69a363ef494363cdf2192a1133c5a158d2bca79
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-11-04 15:18:00 +0200

    Recursive call template

---
+++ TestXml/UserManagerService.Integration/Option/ServiceOption.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagerService.Integration.Option
+{
+    /// <summary>
+    /// Service configuration
+    /// </summary>
+    public class ServiceOption
+    {
+        /// <summary>
+        /// Time describes how often method should be executed (in minutes)
+        /// </summary>
+        public int RecursiveTimeoutMinutes { get; set; }
+    }
+}
--- TestXml/UserManagerService/Services.cs
+++ TestXml/UserManagerService/Services.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using UserManagerService.Integration;
 using UserManagerService.Integration.Interfaces;
+using UserManagerService.Integration.Option;
 using UserManagerService.LoggerFactory;
 
 namespace UserManagerService
@@ -20,8 +24,43 @@ namespace UserManagerService
                 .AddSingleton<IWindowsService, AppWindowsService>()
                 .AddSingleton<IRecursiveService, RecursiveExecutionService>();
 
+            AddConfiguration(services);
+            AddServiceOptions<ServiceOption>(services, "Service");
+
             var result = services.BuildServiceProvider();
             return result;
         }
+
+        private static void AddConfiguration(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var result = builder.Build();
+            services.AddSingleton<IConfiguration>(result);
+        }
+
+        /// <summary>
+        /// Allow us to add additional custom options to the Service
+        /// </summary>
+        /// <typeparam name="TOptions">Name of the class which implement options</typeparam>
+        /// <param name="services">Services</param>
+        /// <param name="sectionName">Name of the section in appsettings file</param>
+        /// <returns></returns>
+        public static IServiceCollection AddServiceOptions<TOptions>(IServiceCollection services, string sectionName) where TOptions : class
+            => services.AddSingleton<TOptions>(sp =>
+            {
+                var configurations = sp.GetRequiredService<IEnumerable<IConfiguration>>();
+                foreach (var configuration in configurations)
+                {
+                    var section = configuration.GetSection(sectionName);
+                    if (!section.Exists()) continue;
+
+                    return section.Get<TOptions>();
+                }
+
+                return default;
+            });
     }
 }
